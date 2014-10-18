8afc5a421fd6952b9b22fba6d4c4ac15a1e73906
wendelad <wendelad89@gmail.com>
2014-10-17 23:58:03 -0300

    Implementação da lista encadeada

--- Trie-Search.c
+++ Trie-Search.c
@@ -9,90 +9,46 @@
 #include <stdbool.h>
 #define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
 int lv=0;
-struct no {
+int linha=1;
+
+typedef struct no {
 	struct no *chave[27]; 
 	char flag;
 }no, *NoTrie;
 
-typedef struct no node;
+//typedef struct no node;
 
-typedef struct Lista{
-    char *palavra;
-    struct Lista *prox;
-}Lista, *PLista;
+typedef struct NolistaInt{
+
+	char *palavra;
+	struct NolistaInt *prox;
+}NolistaInt, *Listaint;
+
+void inicializa(NoTrie *No);
+void inicializaLista(Listaint *l);
+void inserir(NoTrie *No, char *word);
+int busca_palavra(NoTrie *No, char *word);
+bool consultaLista(Listaint *l, char *plv);
+bool insereLista(Listaint *l, char *plv);
+int levenshtein(char *s1, char *s2);
 
 
-void inicializa(node **No){
-	*No = NULL;
-	}
-
-	void inicializaLista(PLista *l){
-	*l = NULL;
-}
-int levenshtein(char *s1, char *s2) {
-    unsigned int x, y, s1len, s2len;
-    s1len = strlen(s1);
-    s2len = strlen(s2);
-    unsigned int matrix[s2len+1][s1len+1];
-    matrix[0][0] = 0;
-    for (x = 1; x <= s2len; x++)
-        matrix[x][0] = matrix[x-1][0] + 1;
-    for (y = 1; y <= s1len; y++)
-        matrix[0][y] = matrix[0][y-1] + 1;
-    for (x = 1; x <= s2len; x++)
-        for (y = 1; y <= s1len; y++)
-            matrix[x][y] = MIN3(matrix[x-1][y] + 1, matrix[x][y-1] + 1, matrix[x-1][y-1] + (s1[y-1] == s2[x-1] ? 0 : 1));
- 
-    return(matrix[s2len][s1len]);
-}
-void inserir(node **No, char *word){
-	int i;
-	
-	if(*No == NULL){
-		
-		*No = malloc(sizeof(node));
-		for(i=0;i<27;i++)
-		(*No)->chave[i]= NULL;
-		
-		if(word[0] != '\0'){
-			inserir(&(*No)->chave[word[0] - 'a'], word +1);
-			(*No)->flag = 0;
-		}else
-			(*No)->flag = 1;
-		}else{
-			 if(word[0] != '\0'){
-				inserir(&(*No)->chave[word[0] - 'a'], word +1);
-			
-			}else
-			(*No)->flag = 1;
-		}
-}
-
-int busca_palavra(node **No, char *word){
-    if(*No == NULL){
-        return 0;
-    }
-    
-    else if(word[0]=='\0')
-        return((*No)->flag ? 1:0);
-        
-    else
-		return busca_palavra(&(*No)->chave[word[0]- 'a'], word+1);
-
-}
 
 int main(int argc, char *argv[]){
+
+	FILE *arq, *arq2;
+	NoTrie raiz;
+	Listaint lst;
 	
 	int j=0,k=0,i=0;
-	int linha=1,tam1,tam2;
+	int tam1,tam2;
 	char ch,ch2;
 	char *palavra;
-	node *raiz;
-	
+
 	inicializa(&raiz);
+	inicializaLista(&lst);
 	
-	FILE *arq, *arq2;
-	
+
 	arq = fopen(argv[2], "r");
 	arq2 = fopen(argv[1], "r");
 
@@ -127,6 +83,7 @@ int main(int argc, char *argv[]){
 				//printf("%s\n",word);
 			}
 		}
+		printf("TESTE");
 
 		
 		if (word[0] != '\0'){
@@ -187,6 +144,98 @@ int main(int argc, char *argv[]){
 	fclose(arq2);
 	return 0;
 }
-/*ler palavra nao encontrada
 
-distancia_entre (palavra , [pdic1 , pdic2 .........] )*/
+//INICIALIZANDO TRIE
+void inicializa(NoTrie *No){
+	*No = NULL;
+}
+
+//INICIALIZANDO LISTA ENCADEADA
+void inicializaLista(Listaint *l){
+
+	(*l) = NULL;
+}
+
+
+//INSERINDO NA TRIE
+void inserir(NoTrie *No, char *word){
+	int i;
+	
+	if(*No == NULL){
+		
+		*No = malloc(sizeof(no));
+		for(i=0;i<27;i++)
+		(*No)->chave[i]= NULL;
+		
+		if(word[0] != '\0'){
+			inserir(&(*No)->chave[word[0] - 'a'], word +1);
+			(*No)->flag = 0;
+		}else
+			(*No)->flag = 1;
+		}else{
+			 if(word[0] != '\0'){
+				inserir(&(*No)->chave[word[0] - 'a'], word +1);
+			
+			}else
+			(*No)->flag = 1;
+		}
+}
+
+//BUSCANDO NA TRIE
+int busca_palavra(NoTrie *No, char *word){
+    if(*No == NULL){
+        return 0;
+    }
+    
+    else if(word[0]=='\0')
+        return((*No)->flag ? 1:0);
+        
+    else
+		return busca_palavra(&(*No)->chave[word[0]- 'a'], word+1);
+
+}
+
+
+//CONSULTA NA LISTA
+bool consultaLista(Listaint *l, char *plv){
+
+	Listaint p;
+
+	for(p=(*l); (p) && (p->palavra != plv);p=p->prox);
+			
+		return p;	
+}
+
+//INSERINDO NA LISTA
+bool insereLista(Listaint *l, char *plv){
+
+	NolistaInt *p;
+
+	if(consultaLista(l,plv)||! (p=((NolistaInt*)malloc(sizeof(NolistaInt))))){
+		
+		return false;
+	}
+	p->prox = *l;
+	*l = p;
+	p->palavra = plv;
+	
+	return true;
+}
+
+//LEVENSTHEIN
+int levenshtein(char *s1, char *s2) {
+    unsigned int x, y, s1len, s2len;
+    s1len = strlen(s1);
+    s2len = strlen(s2);
+    unsigned int matrix[s2len+1][s1len+1];
+    matrix[0][0] = 0;
+    for (x = 1; x <= s2len; x++)
+        matrix[x][0] = matrix[x-1][0] + 1;
+    for (y = 1; y <= s1len; y++)
+        matrix[0][y] = matrix[0][y-1] + 1;
+    for (x = 1; x <= s2len; x++)
+        for (y = 1; y <= s1len; y++)
+            matrix[x][y] = MIN3(matrix[x-1][y] + 1, matrix[x][y-1] + 1, matrix[x-1][y-1] + (s1[y-1] == s2[x-1] ? 0 : 1));
+ 
+    return(matrix[s2len][s1len]);
+}
